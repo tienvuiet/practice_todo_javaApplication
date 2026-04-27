644a4d1bb645105071cadca1a7bd86b719d7db51
Tien <tienxinhzai241@gmail.com>
2026-04-27 17:03:59 +0700

    day3

---
+++ src/main/java/com/example/day1/config/I18NConfig.java
@@ -0,0 +1,37 @@
+package com.example.day1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.time.Duration;
+import java.util.Locale;
+
+@Configuration
+public class I18NConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver clr = new CookieLocaleResolver();
+        clr.setDefaultLocale(new Locale("vi"));
+        clr.setCookieMaxAge(Duration.ofDays(30));
+        return clr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+}
